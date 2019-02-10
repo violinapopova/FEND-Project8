04cd4e71d04188eb42f8f607a8fc5ae905682640
violinapopova <violipopova@gmail.com>
2019-02-10 21:32:49 +0100

    Add files via upload

---
+++ build/precache-manifest.2c02a341352b79ca102bcdf5db079c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfbed76cb1eaf8100329",
+    "url": "/static/js/main.dfbed76c.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "dfbed76cb1eaf8100329",
+    "url": "/static/css/main.278144cf.chunk.css"
+  },
+  {
+    "revision": "b80742fde374e18d3904a206070045ce",
+    "url": "/index.html"
+  }
+];
